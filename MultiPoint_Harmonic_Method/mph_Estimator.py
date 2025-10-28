c88becb32d8739c891dfc4c5b6d1b3823053e097
Dr. Daniel S. Stutts <stutts@mst.edu>
2025-10-27 20:02:16 -0500

    Added request for user citation.

--- MultiPoint_Harmonic_Method/mph_Estimator.py
+++ MultiPoint_Harmonic_Method/mph_Estimator.py
@@ -43,6 +43,9 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
+#
+# The authors request that if you use this code, please kindly cite the aforementioned paper by Tomanek and Stutts.
+#
 ###################################################
 
 import sys
